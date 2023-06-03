9840549ecdea365497b48bbf6f8567ba9c4acabf
Hamza <hamza.javed@koderlabs.com>
2023-06-03 21:08:42 +0500

    add swaggar

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [TodoModule, UserModule, AuthModule],
+  imports: [TodoModule, UserModule, AuthModule, PrismaModule],
 })
 export class AppModule {}
